a315fa3e18d719e0d1e61642afe66ff88a4b0c3a
ryancharley <ryancharley429@gmail.com>
2021-04-19 05:21:45 -0400

    add breakdown pop up

---
+++ src/pages/segments/BreakDown.tsx
@@ -0,0 +1,34 @@
+import {
+	Box,
+	Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+} from "@chakra-ui/react";
+
+export const BreakDown: React.FC = () => {
+	return (
+		<Box className="breakdown">
+			<Popover
+				placement="top"
+				trigger="hover"
+			>
+				<PopoverTrigger>
+					<div className="breakdown-bar"></div> 
+				</PopoverTrigger>
+				<PopoverContent>
+					<PopoverArrow />
+					<PopoverBody>
+						<div className="text-green">Realized: 0 (0%)</div>
+						<div className="text-blue">Existing: 0 (0%)</div>
+						<div className="text-red">Existing: 0 (0%)</div>
+					</PopoverBody>
+				</PopoverContent>
+			</Popover>
+		</Box>
+	);
+}
--- src/pages/segments/BrowsePanel.tsx
+++ src/pages/segments/BrowsePanel.tsx
@@ -1,28 +1,173 @@
 import {
 	Box,
+	Flex,
 	Input,
 	InputGroup,
 	InputLeftElement,
-	Icon
+	Icon,
+	IconButton,
+	Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
 } from "@chakra-ui/react";
 import { RiSearchLine } from 'react-icons/ri';
-
+import { MdFilterList } from "react-icons/md";
+import { BreakDown } from "./BreakDown";
 
 export const BrowsePanel: React.FC = () => {
 	return (
 		<Box>
-			<InputGroup>
-				<InputLeftElement
-					pointerEvents="none"
-					children={<Icon as={RiSearchLine} fontSize="15" />}
-				/>
-				<Input
-					width="300px"
-					className="search-box"
-					variant="outline"
-					colorScheme="teal"
-					placeholder="Search" />
-			</InputGroup>
+			<Flex>
+				<InputGroup>
+					<InputLeftElement
+						pointerEvents="none"
+						children={<Icon as={RiSearchLine} fontSize="15" />}
+					/>
+					<Input
+						width="300px"
+						className="search-box"
+						variant="outline"
+						colorScheme="teal"
+						placeholder="Search" />
+				</InputGroup>
+				<IconButton
+          aria-label="Filter"
+          icon={<Icon as={MdFilterList} boxSize={18} />}
+          variant="ghost"
+        />
+			</Flex>
+			<Box flex="1" mt="2">
+        <Table bg="white">
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Breakdown</Th>
+              <Th>Churn</Th>
+              <Th>Profile Count</Th>
+              <Th>Evaluation method</Th>
+              <Th>Created</Th>
+              <Th>Last Updated</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            <Tr>
+              <Td>Use Case Segment-5a00b724-cfe5-4a54-9593-06c2e89a1310</Td>
+              <Td>
+                <BreakDown />
+              </Td>
+              <Td>0%</Td>
+              <Td>0</Td>
+              <Td>Streaming</Td>
+              <Td>3/15/2021, 8:01 AM</Td>
+              <Td>4/5/2021, 8:01 AM</Td>
+            </Tr>
+            
+          </Tbody>
+        </Table>
+      </Box>
 		</Box>
 	);
 }
